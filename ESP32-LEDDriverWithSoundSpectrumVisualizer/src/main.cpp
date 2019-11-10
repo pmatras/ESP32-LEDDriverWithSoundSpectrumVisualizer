05ec01fbe1302f160da5e91bf704365b9fece144
birdman98 <piotrmatras21@gmail.com>
2019-11-10 12:40:11 +0100

    added initialization of GPIO pin 33 to input of microphone

--- ESP32-LEDDriverWithSoundSpectrumVisualizer/src/main.cpp
+++ ESP32-LEDDriverWithSoundSpectrumVisualizer/src/main.cpp
@@ -2,6 +2,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+const int MICROPHONE_PIN = 33;
 const int SCREEN_WIDTH = 128; 
 const int SCREEN_HEIGHT = 64;
 
@@ -10,7 +11,8 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET_PIN);
 
 void setup() {
    Serial.begin(115200);
-
+   pinMode(MICROPHONE_PIN, INPUT);
+   
    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
       Serial.println(F("SSD1306 allocation failed"));
       return;
