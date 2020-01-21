b26101a08ef9672f57f22277c7f6917072b3be8d
birdman98 <piotrmatras21@gmail.com>
2020-01-21 18:38:18 +0100

    added constructors to structure

--- ESP32-LEDDriverWithSoundSpectrumVisualizer/src/NetworkConnectivity.h
+++ ESP32-LEDDriverWithSoundSpectrumVisualizer/src/NetworkConnectivity.h
@@ -9,6 +9,18 @@ struct RGBColor {
    int R = 0;
    int G = 0;
    int B = 0;
+
+   RGBColor() {
+      R = 0;
+      G = 0;
+      B = 0;
+   }
+
+   RGBColor(const int &_R, const int &_G, const int &_B) {
+      R = _R;
+      G = _G;
+      B = _B;
+   }
 };
 
 bool connectToWiFi(const char* WiFi_SSID, const char* WiFi_PASSWD, Adafruit_SSD1306 &display);
